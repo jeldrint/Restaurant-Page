689935b0b54c7aabea35e0cbd3678c23e7c51f93
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2023-03-06 20:17:15 +0800

    still on home-page (35%)

--- src/1-home-page.js
+++ src/1-home-page.js
@@ -1,7 +1,6 @@
 import Background from './meryenda.jpg';
 
 const homePage = () => {
-    const container = document.querySelector('#content');
     homePageHeader();
 }
 
@@ -9,17 +8,29 @@ const homePageHeader = () => {
     const container = document.querySelector('#content');
     const header = document.createElement('div');
     header.className = 'page-header';
+    container.appendChild(header);
 
     const textHeader = document.createElement('span');
     textHeader.className = 'text-header';
     textHeader.innerText = "Meryenda ni Mami";
+    header.appendChild(textHeader);
 
     const menuHeader = document.createElement('ul');
     menuHeader.className = 'menu-header';
-    const menuItems = document.createElement('li');
-    menuItems.cr
-
-    container.appendChild(header);
-    header.appendChild(textHeader);
+    header.appendChild(menuHeader);
+    
+    for (let i=0; i<3; i++){
+        let x = document.createElement('li')
+        menuHeader.appendChild(x);
+        
+        switch (i){
+            case 0: menuHeader.children[i].innerText = 'Home';
+                    break;
+            case 1: menuHeader.children[i].innerText = 'Menu';
+                    break;
+            case 2: menuHeader.children[i].innerText = 'Contact';
+                    break;
+        }
+    }
 }
 export default homePage;
